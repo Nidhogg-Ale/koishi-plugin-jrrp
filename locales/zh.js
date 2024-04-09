8a76b13b39cf9dd7a9016e2264d7536cc8578ce5
Nidhogg-Ale <56574860+Nidhogg-Ale@users.noreply.github.com>
2024-04-09 18:45:33 +0800

    Update zh.js

--- locales/zh.js
+++ locales/zh.js
@@ -6,17 +6,17 @@ const Locales = {
     },
   },
   jrrp: {
-    'result': '{0} 的今日人品是：{1}。{2}',
+    'result': '沫纯猜测{0} 的今日人品是：{1}喵w~。{2}',
 
-    'default-level-0': '推荐闷头睡大觉。',
+    'default-level-0': '方便面少调料包，外卖少餐具喵w~。',
     'default-level-20': '也许今天适合摆烂。',
     'default-level-40': '又是平凡的一天。',
-    'default-level-60': '太阳当头照，花儿对你笑。',
-    'default-level-80': '出门可能捡到 1 块钱。',
+    'default-level-60': '运气还不错喵w~',
+    'default-level-80': '今天适合抽卡喵w~。',
 
     'default-jackpot-0': '怎，怎么会这样……',
-    'default-jackpot-42': '感觉可以参透宇宙的真理。',
-    'default-jackpot-77': '要不要去抽一发卡试试呢……？',
+    'default-jackpot-42': '-the Answer to Life, the Universe and Everything- is 42。',
+    'default-jackpot-69': '要不要和沫纯来一发试试呢……？',
     'default-jackpot-100': '买彩票可能会中大奖哦！',
   },
 }
